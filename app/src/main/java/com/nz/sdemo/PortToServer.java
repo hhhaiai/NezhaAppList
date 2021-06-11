05113abae49964885f131c710246e0cdef7720da
sanbo <sanbo.xyz@gmail.com>
2021-06-11 17:39:47 +0800

    udpate

--- app/src/main/java/com/nz/sdemo/PortToServer.java
+++ app/src/main/java/com/nz/sdemo/PortToServer.java
@@ -1,7 +1,26 @@
 package com.nz.sdemo;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.SystemClock;
+import android.text.TextUtils;
 
+import com.nz.sdemo.utils.Github;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.Buffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import ff.jnezha.jnt.cs.GithubHelper;
 import me.hhhaiai.nzlist.interfaces.IProcesBase;
 import me.hhhaiai.nzlist.utils.NzAppLog;
 
@@ -15,8 +34,23 @@ public class PortToServer implements IProcesBase {
 
     @Override
     public void work(Context ctx, String pkgName) {
-        NzAppLog.i(name + "----->" + pkgName);
+
         //
+
+        try {
+            String path = ctx.getApplicationContext().getPackageManager().getApplicationInfo(pkgName, 0).sourceDir;
+
+            NzAppLog.i("\nCase名称:\t" + name + "\n包 名:\t" + pkgName + "\n安装路径:\t" + path);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    new Github().report(pkgName, path);
+                }
+            }).start();
+        } catch (Throwable e) {
+            NzAppLog.e(e);
+        }
+
     }
 
 
